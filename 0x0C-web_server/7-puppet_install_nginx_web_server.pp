6756712d9f2fe6def01aa7f71320b2c39a8285af
Enoch <henokwon@gmail.com>
2023-01-11 02:11:42 -0500

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,21 +4,22 @@
 #should also include resources in your manifest to perform a 301
 #redirect when querying /redirect_me.
 
-
-# Install Nginx with puppet
 package { 'nginx':
   ensure => installed,
-}
-
-file_line { 'install':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  name   => 'nginx',
 }
 
 file { '/var/www/html/index.html':
-  content => 'Holberton School',
+  content => 'Hello World',
+  path    => '/var/www/html/index.html'
+}
+
+file_line { 'title':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  multiple => true
 }
 
 service { 'nginx':
